835e811bbf2bc804a76bcbf58798ee0f58412bce
priyamesh <priyamesh28@gmail.com>
2022-06-29 16:07:56 +0530

    basic setup for project

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,12 @@
+from urllib import response
 from django.test import TestCase
+from django.urls import reverse_lazy
 
 # Create your tests here.
+
+
+class HomeTest(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse_lazy("home")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
